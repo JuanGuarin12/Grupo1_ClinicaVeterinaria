a4c62abaee4467d8d506c01ce570fa2b8c698ec5
Jeonnayu <nacs1815@gmail.com>
2026-09-04 18:35:55 +0200

    Agrega la clase vacuna con sus atributos y sus metodos

---
+++ src/com/itm/clinicaveterinaria/models/domain/Vacuna.java
@@ -0,0 +1,41 @@
+
+import java.time.LocalDate;
+
+public class Vacuna {
+        private String nombre;
+        private LocalDate fechaAplicacion;
+        private LocalDate proximaFecha;
+        public Vacuna(String nombre, LocalDate fechaAplicacion, LocalDate proximaFecha) {
+                this.nombre = nombre;
+                this.fechaAplicacion = fechaAplicacion;
+                this.proximaFecha = proximaFecha;
+        }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFechaAplicacion() {
+        return fechaAplicacion;
+    }
+
+    public void setFechaAplicacion(LocalDate fechaAplicacion) {
+        this.fechaAplicacion = fechaAplicacion;
+    }
+
+    public LocalDate getProximaFecha() {
+        return proximaFecha;
+    }
+
+    public void setProximaFecha(LocalDate proximaFecha) {
+        this.proximaFecha = proximaFecha;
+    }
+        
+        
+
+
+}
