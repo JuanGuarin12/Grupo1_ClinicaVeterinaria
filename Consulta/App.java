75a456855c0e44ee41848ce79a0387be00bdd1b6
Miguel <miguel.agumol@gmail.com>
2026-09-08 19:03:21 -0500

    Implementa atributos, getters y setters, constructor

--- Consulta/App.java
+++ Consulta/App.java
@@ -1,9 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-    
-        Consulta Miguel = new Consulta("Medellin", 3, 59.24);
-        Miguel.datosConsulta();
-    }
 
-    
+}
 }
--- Consulta/Consulta.java
+++ Consulta/Consulta.java
@@ -1,24 +1,41 @@
+import java.time.LocalDate;
+
 public class Consulta { 
-    //Definimos atributos de la clase Consulta
-    String sede;
-    Double precio;
-    int hora;
     
-    //Definimos el metodo de la clase Consulta
-    void datosConsulta(){
-    System.out.println("sede: " + this.sede);
-    System.out.println("hora:" + this.hora);
-    System.out.println("precio: " + this.precio);
+    private String motivo;
+    private String diagnostico;
+    private String tratamiento;
+    private LocalDate Fecha;
+
+    public String getMotivo() {
+        return motivo;
+    }
+    public void setMotivo(String motivo) {
+        this.motivo = motivo;
+    }
+    public String getDiagnostico() {
+        return diagnostico;
+    }
+    public void setDiagnostico(String diagnostico) {
+        this.diagnostico = diagnostico;
+    }
+    public String getTratamiento() {
+        return tratamiento;
+    }
+    public void setTratamiento(String tratamiento) {
+        this.tratamiento = tratamiento;
+    }
+    public LocalDate getFecha() {
+        return Fecha;
+    }
+    public void setFecha(LocalDate fecha) {
+        Fecha = fecha;
     }
 
-    //Definimos el metodo constructor de la clase Consulta
-    public Consulta(String sede, int hora, Double precio){
-        this.sede = sede;
-        this.precio = precio;
-        this.hora = hora;
+    public Consulta(String motivo, String diagnostico, String tratamiento, LocalDate fecha) {
+        this.motivo = motivo;
+        this.diagnostico = diagnostico;
+        this.tratamiento = tratamiento;
+        this.Fecha = fecha;
     }
-
-
-
-
-    }
+}
